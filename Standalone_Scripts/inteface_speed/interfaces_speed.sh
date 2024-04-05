c2040fcb1ee1cfcf25c6d23cece5d843c5d0d1e2
Thomas Vincent <thomasvincent@gmail.com>
2024-04-05 01:41:47 -0700

    Updated to pass spellcheck

--- Standalone_Scripts/inteface_speed/interfaces_speed.sh
+++ Standalone_Scripts/inteface_speed/interfaces_speed.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-INTERFACE_LIST=`ifconfig | grep '^[a-z0-9]' | awk '{print $1}'`
+# Get a list of network interfaces (excluding loopback)
+interfaces=( $(ifconfig | grep -Ev '^lo:' | awk '{print $1}') )
 
-arr=$(echo $INTERFACE_LIST | tr " " "\n")
+# Loop through each interface
+for interface in "${interfaces[@]}"; do
+  # Get the link speed using ethtool (handle potential errors)
+  speed=$(ethtool "$interface" | grep Speed | awk -F':' '{print $2}' || true)
 
-for x in $arr
-do
-    SPEED=`ethtool $x | grep Speed | tr -d '[A-Za-z]' | tr -d ' ' | tr -d ':' | tr -d '/'  `
-    if [ $SPEED ] ; then
-	echo "interface "$x "6" $(($SPEED * 1000000))
-    fi
+  # Check if speed is retrieved successfully
+  if [[ -n "$speed" ]]; then
+    # Convert speed to Mbps (assuming Mbit/s format)
+    mbps=$(( speed * 1000000 ))
+    echo "interface $interface 6 $mbps"  # Assuming format "interface <name> <duplex> <speed>"
+  fi
 done
