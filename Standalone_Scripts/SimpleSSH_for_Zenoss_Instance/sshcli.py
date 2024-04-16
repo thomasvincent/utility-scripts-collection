a74c18c10f9499056c5e754e2efc19b6a903404b
Thomas Vincent <thomasvincent@gmail.com>
2024-04-16 01:44:36 -0700

    Update to python3

--- Standalone_Scripts/SimpleSSH_for_Zenoss_Instance/sshcli.py
+++ Standalone_Scripts/SimpleSSH_for_Zenoss_Instance/sshcli.py
@@ -1,36 +1,39 @@
-#!/opt/zenoss/bin/python
+#!/usr/bin/env python3
 
 from twisted.conch import error
 from twisted.conch.ssh import transport, connection, keys, userauth, channel, common
 from twisted.internet import defer, protocol, reactor
+import sys
+import getpass
 
 class ClientCommandTransport(transport.SSHClientTransport):
-    def __init__(self, username, password, command):
+    def __init__(self, username: str, password: str, command: str):
+        super().__init__()
         self.username = username
         self.password = password
         self.command = command
 
-    def verifyHostKey(self, pubKey, fingerprint):
-        # in a real app, you should verify that the fingerprint matches
+    def verifyHostKey(self, pubKey: bytes, fingerprint: str) -> defer.Deferred:
+        # In a real app, you should verify that the fingerprint matches
         # the one you expected to get from this server
         return defer.succeed(True)
 
     def connectionSecure(self):
         self.requestService(
             PasswordAuth(self.username, self.password,
-                ClientConnection(self.command)))
+                         ClientConnection(self.command)))
 
 class PasswordAuth(userauth.SSHUserAuthClient):
-    def __init__(self, user, password, connection):
-        userauth.SSHUserAuthClient.__init__(self, user, connection)
+    def __init__(self, user: str, password: str, connection: protocol.Protocol):
+        super().__init__(user, connection)
         self.password = password
 
-    def getPassword(self, prompt=None):
+    def getPassword(self, prompt: str = None) -> defer.Deferred:
         return defer.succeed(self.password)
 
 class ClientConnection(connection.SSHConnection):
-    def __init__(self, cmd, *args, **kwargs):
-        connection.SSHConnection.__init__(self)
+    def __init__(self, cmd: str):
+        super().__init__()
         self.command = cmd
 
     def serviceStarted(self):
@@ -39,11 +42,11 @@ class ClientConnection(connection.SSHConnection):
 class CommandChannel(channel.SSHChannel):
     name = 'session'
 
-    def __init__(self, command, *args, **kwargs):
-        channel.SSHChannel.__init__(self, *args, **kwargs)
+    def __init__(self, command: str, **kwargs):
+        super().__init__(**kwargs)
         self.command = command
 
-    def channelOpen(self, data):
+    def channelOpen(self, data: bytes):
         self.conn.sendRequest(
             self, 'exec', common.NS(self.command), wantReply=True).addCallback(
             self._gotResponse)
@@ -51,30 +54,32 @@ class CommandChannel(channel.SSHChannel):
     def _gotResponse(self, _):
         self.conn.sendEOF(self)
 
-    def dataReceived(self, data):
-        print data
+    def dataReceived(self, data: bytes):
+        print(data.decode())
 
     def closed(self):
         reactor.stop()
 
 class ClientCommandFactory(protocol.ClientFactory):
-    def __init__(self, username, password, command):
+    def __init__(self, username: str, password: str, command: str):
         self.username = username
         self.password = password
         self.command = command
 
     def buildProtocol(self, addr):
-        protocol = ClientCommandTransport(
+        return ClientCommandTransport(
             self.username, self.password, self.command)
-        return protocol
 
 if __name__ == "__main__":
-    import sys, getpass
-    server = sys.argv[1]
-    command = sys.argv[2]
-    username = raw_input("Username: ")
-    password = getpass.getpass("Password: ")
-    factory = ClientCommandFactory(username, password, command)
-    reactor.connectTCP(server, 22, factory)
-    reactor.run()
-
+    try:
+        server = sys.argv[1]
+        command = sys.argv[2]
+        username = input("Username: ")
+        password = getpass.getpass("Password: ")
+        factory = ClientCommandFactory(username, password, command)
+        reactor.connectTCP(server, 22, factory)
+        reactor.run()
+    except IndexError:
+        print("Usage: script.py <server> <command>")
+    except Exception as e:
+        print(f"An error occurred: {e}")
